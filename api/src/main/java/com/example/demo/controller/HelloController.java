810726e12f88c0bfef1a6f01d33003d1cb4901cc
Carmela <carmela_mm@hotmail.com.ar>
2021-12-23 02:35:15 -0300

    Agrego hola.html al directorio raíz /

--- api/src/main/java/com/example/demo/controller/HelloController.java
+++ api/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,14 +1,14 @@
 package com.example.demo.controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 	
-	@GetMapping("hello")
+	@GetMapping("/")
 	public String hello() {
 		
-		return "Hola CODEALO CAMP";
+		return "hola";
 	}
 	
 
