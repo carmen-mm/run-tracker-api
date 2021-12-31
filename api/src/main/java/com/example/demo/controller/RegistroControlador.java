b366b53d944ffe02867a2f37cc2db08166804b40
Carmela <carmela_mm@hotmail.com.ar>
2021-12-31 01:32:32 -0300

    Agrego controladores

---
+++ api/src/main/java/com/example/demo/controller/RegistroControlador.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.service.UsuarioServicio;
+
+@Controller
+public class RegistroControlador {
+
+	@Autowired
+	private UsuarioServicio servicio;
+	
+	@GetMapping("/login")
+	public String iniciarSesion() {
+		return "login";
+	}
+	
+	@GetMapping("/")
+	public String verPaginaDeInicio(Model modelo) {
+		modelo.addAttribute("usuario", servicio.listarUsuarios());
+		return "index";
+	}
+}
---
+++ api/src/main/java/com/example/demo/controller/RegistroUsuarioControlador.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.dto.UsuarioRegistroDto;
+import com.example.demo.service.UsuarioServicio;
+
+@Controller
+@RequestMapping("/registro")
+public class RegistroUsuarioControlador {
+
+	private UsuarioServicio usuarioServicio;
+
+	public RegistroUsuarioControlador(UsuarioServicio usuarioServicio) {
+		super();
+		this.usuarioServicio = usuarioServicio;
+	}
+	
+	@ModelAttribute("usuario")
+	public UsuarioRegistroDto retornarNuevoUsuarioRegistroDTO() {
+		return new UsuarioRegistroDto();
+	}
+
+	@GetMapping
+	public String mostrarFormularioDeRegistro() {
+		return "registro";
+	}
+	
+	@PostMapping
+	public String registrarCuentaDeUsuario(@ModelAttribute("usuario") UsuarioRegistroDto registroDTO) {
+		usuarioServicio.guardar(registroDTO);
+		return "redirect:/registro?exito";
+	}
+}
