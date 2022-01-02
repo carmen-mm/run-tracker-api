622b2aeeab33a7eb3b588618f5ddb279ae0aa69f
Carmela <carmela_mm@hotmail.com.ar>
2022-01-02 18:14:00 -0300

    agrego atributo rol y cambio contraseña por password

--- api/src/main/java/com/example/demo/dto/UsuarioRegistroDto.java
+++ api/src/main/java/com/example/demo/dto/UsuarioRegistroDto.java
@@ -5,7 +5,8 @@ public class UsuarioRegistroDto {
 	private Long id;
 	private String nombre;
 	private String email;
-	private String contraseña;
+	private String password;
+	private String rol;
 	
 	public Long getId() {
 		return id;
@@ -19,11 +20,11 @@ public class UsuarioRegistroDto {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public String getContraseña() {
-		return contraseña;
+	public String getPassword() {
+		return password;
 	}
-	public void setContraseña(String contraseña) {
-		this.contraseña = contraseña;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	public String getEmail() {
 		return email;
@@ -31,26 +32,29 @@ public class UsuarioRegistroDto {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public UsuarioRegistroDto(Long id, String nombre, String email,String contraseña) {
-		super();
+	public UsuarioRegistroDto(Long id, String nombre, String email,String password) {
 		this.id = id;
 		this.nombre = nombre;
 		this.email = email;
-		this.contraseña = contraseña;
+		this.password = password;
 	}
-	public UsuarioRegistroDto(String nombre,String email, String contraseña) {
-		super();
+	public UsuarioRegistroDto(String nombre,String email, String password) {
 		this.nombre = nombre;
 		this.email = email;
-		this.contraseña = contraseña;
+		this.password = password;
 	}
 
 	public UsuarioRegistroDto() {
 	
 	}
 	public UsuarioRegistroDto(String email) {
-		super();
 		this.email = email;
+	}
+	public String getRol() {
+		return rol;
+	}
+	public void setRol(String rol) {
+		this.rol = rol;
 	}
 	
 	
