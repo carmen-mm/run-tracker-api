74d55a3a99dbab544675a4b7f2562c46d29b07fc
Carmela <carmela_mm@hotmail.com.ar>
2022-01-07 01:46:58 -0300

    Creo Repositorio para Rol

--- api/src/main/java/com/example/demo/service/UsuarioServicioImpl.java
+++ api/src/main/java/com/example/demo/service/UsuarioServicioImpl.java
@@ -1,53 +1,54 @@
 package com.example.demo.service;
 
-import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.authority.AuthorityUtils;
 
 import com.example.demo.dto.UsuarioRegistroDto;
 import com.example.demo.model.Rol;
 import com.example.demo.model.Usuario;
+import com.example.demo.repository.RolRepository;
 import com.example.demo.repository.UsuarioRepository;
 
 @Service
 public class UsuarioServicioImpl implements UsuarioServicio{
 
 	private UsuarioRepository usuarioRepository;
+	private RolRepository rolRepository;
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 	
-	public UsuarioServicioImpl(UsuarioRepository usuarioRepository) {
+	public UsuarioServicioImpl(UsuarioRepository usuarioRepository, RolRepository rolRepository ) {
 		super();
+		this.rolRepository = rolRepository;
 		this.usuarioRepository = usuarioRepository;
 	}
 
 	@Override
 	public Usuario guardar(UsuarioRegistroDto registroDTO) {
-		Usuario usuario = new Usuario(registroDTO.getNombre(), registroDTO.getEmail(), passwordEncoder.encode(registroDTO.getContraseña()),new Rol("ADMIN"));
+		Rol rol = this.rolRepository.findRol(registroDTO.getNombre_rol());
+		Usuario usuario = new Usuario(registroDTO.getNombre(), registroDTO.getEmail(), passwordEncoder.encode(registroDTO.getPassword()), rol );
+		
 		return usuarioRepository.save(usuario);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Usuario usuario = usuarioRepository.findByEmail(username);
 		if(usuario == null) {
 			throw new UsernameNotFoundException("Usuario o contraseña inválidos");
 		}
-		return new User(usuario.getEmail(),usuario.getContraseña(), (Collection<? extends GrantedAuthority>) usuario.getRol());
+		return new User(usuario.getEmail(), usuario.getPassword(), AuthorityUtils.commaSeparatedStringToAuthorityList("CORREDOR"));
 	}
 
-/*	private Collection<? extends GrantedAuthority> mapearAutoridadesRoles(Rol rol){
-		return ((Object) rol.stream()).map(rol -> new SimpleGrantedAuthority(rol.getNombre())).collect(Collectors.toList());
-	}*/
+	
 	
 	@Override
 	public List<Usuario> listarUsuarios() {
