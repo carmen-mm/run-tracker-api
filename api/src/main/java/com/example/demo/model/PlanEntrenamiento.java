f629ccfa0bd85bd3270b97e12e60f3fbdcfe611f
Carmela <carmela_mm@hotmail.com.ar>
2022-01-20 00:59:38 -0300

    Modifico tablas

--- api/src/main/java/com/example/demo/model/PlanEntrenamiento.java
+++ api/src/main/java/com/example/demo/model/PlanEntrenamiento.java
@@ -1,11 +1,17 @@
 package com.example.demo.model;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 
@@ -20,7 +26,16 @@ public class PlanEntrenamiento {
 	private String nombre;
 	private Long kms;
 	private String observaciones;
-	private Collection<Sesion> sesiones;
+	private Date fecha_inicio;
+	//private Usuario corredor;	
+	
+	@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "plan_sesiones",
+            joinColumns = @JoinColumn(name = "plan_id"),
+            inverseJoinColumns = @JoinColumn(name = "sesion_id")
+    )
+    private List<Sesion> sesiones = new ArrayList<>();
+
 	
 	public PlanEntrenamiento() {
 	
@@ -49,6 +64,30 @@ public class PlanEntrenamiento {
 	}
 	public void setObservaciones(String observaciones) {
 		this.observaciones = observaciones;
+	}
+/*
+	public Usuario getEntrenador() {
+		return entrenador;
+	}
+
+	public void setEntrenador(Usuario entrenador) {
+		this.entrenador = entrenador;
+	}
+*/
+	public List<Sesion> getSesiones() {
+		return sesiones;
+	}
+
+	public void setSesiones(List<Sesion> sesiones) {
+		this.sesiones = sesiones;
+	}
+
+	public Date getFecha_inicio() {
+		return fecha_inicio;
+	}
+
+	public void setFecha_inicio(Date fecha_inicio) {
+		this.fecha_inicio = fecha_inicio;
 	}
 
 }
--- api/src/main/java/com/example/demo/model/Sesion.java
+++ api/src/main/java/com/example/demo/model/Sesion.java
@@ -1,10 +1,15 @@
 package com.example.demo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
 
 @Entity // Esto le dice a Hibernate que haga una tabla de esta clase
 @Table(name = "sesion")
@@ -15,26 +20,28 @@ public class Sesion {
 	private Long id;
 	
 	private String nombre;
-	private Long km_diario;
+	private Float km_diario;
 	private String tiempo;
 	private String indicaciones;
-	private Boolean listo;
+	private Boolean estado;
+	
+	@ManyToMany(mappedBy = "sesiones")
+    private List<PlanEntrenamiento> planes = new ArrayList<>();
 	
 	public Sesion() {
 
 	}
-	public Sesion(String nombre, Long km_diario, String tiempo, String indicaciones, Boolean listo) {
-		super();
+	public Sesion(String nombre, Float km_diario, String tiempo, String indicaciones, Boolean estado) {
 		this.nombre = nombre;
 		this.km_diario = km_diario;
 		this.tiempo = tiempo;
 		this.indicaciones = indicaciones;
-		this.listo = listo;
+		this.estado = estado;
 	}
-	public Long getKm_diario() {
+	public Float getKm_diario() {
 		return km_diario;
 	}
-	public void setKm_diario(Long km_diario) {
+	public void setKm_diario(Float km_diario) {
 		this.km_diario = km_diario;
 	}
 	public String getTiempo() {
@@ -49,11 +56,11 @@ public class Sesion {
 	public void setIndicaciones(String indicaciones) {
 		this.indicaciones = indicaciones;
 	}
-	public Boolean getListo() {
-		return listo;
+	public Boolean getEstado() {
+		return estado;
 	}
-	public void setListo(Boolean listo) {
-		this.listo = listo;
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
 	}
 	public String getNombre() {
 		return nombre;
