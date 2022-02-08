fd8b73935e2cc243899e17b2ba405f947874d9f5
Carmela <carmela_mm@hotmail.com.ar>
2022-02-08 01:38:52 -0300

    @RestController @RequestMapping()

--- api/src/main/java/com/example/demo/controller/PlanEntrenamientoControlador.java
+++ api/src/main/java/com/example/demo/controller/PlanEntrenamientoControlador.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,29 +9,42 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.PlanEntrenamientoServicio;
 import com.example.demo.model.PlanEntrenamiento;
 
-@Controller
+@RestController
 @RequestMapping("/planes")
 public class PlanEntrenamientoControlador {
 	@Autowired
 	PlanEntrenamientoServicio planEntrenamientoServicio;
 		
-	@GetMapping(value="/obtenerPlanes")
-	public String obtenerPlanes(Model modelo){
-		List<PlanEntrenamiento> planes=planEntrenamientoServicio.obtenerPlanes();
-		modelo.addAttribute("planes",planes);
-		return "obtenerPlanes";
+	@RequestMapping(value="/obtenerPlanes", method=RequestMethod.GET)
+	public List<PlanEntrenamiento> obtenerPlanes(Model modelo){
+		return planEntrenamientoServicio.obtenerPlanes();
+		
+		
+//		List<PlanEntrenamiento> planes=planEntrenamientoServicio.obtenerPlanes();
+//		modelo.addAttribute("planes",planes);
+//		try {
+//			return "obtenerPlanes";
+//			
+//		}catch (Exception e) {
+//			return ("Error: "+e);
+//		}
+}	
 	
-	}
-
-	 @PostMapping(value="/guardar")
-	 public PlanEntrenamiento guardarPlan(@RequestBody PlanEntrenamiento plan){
+	@RequestMapping(value="/obtenerUno/{id}", method=RequestMethod.GET)
+	public Optional<PlanEntrenamiento> obtenerUno(@PathVariable Long id ){
+			return planEntrenamientoServicio.obtenerUno(id);
+	
+	 }
+	
+	 @RequestMapping(value="/guardar", method=RequestMethod.POST)
+	 public PlanEntrenamiento guardar(PlanEntrenamiento plan){
 	        return this.planEntrenamientoServicio.guardarPlan(plan);
 	}
 	 
